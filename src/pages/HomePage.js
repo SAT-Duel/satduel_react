03091f27156f5632afe6ac16de8b62b5e2bd3885
FarmerJohnsBessie <cz52013141834@gmail.com>
2024-06-26 13:54:34 -0700

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+function HomePage() {
+    return (
+        <div>
+            <Navbar/>
+            <h1>Welcome to the SAT Practice Website!</h1>
+            <p>Enhance your SAT skills with our tailored practice sessions and mock tests.</p>
+            <div>
+                <Link to="/practice">
+                    <button>Start Practice</button>
+                </Link>
+                <Link to="/mock-test">
+                    <button>Mock Test</button>
+                </Link>
+                <Link to="/review">
+                    <button>Review Answers</button>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default HomePage;
